f4d3cc6583cc2acbc2424893ead188b458b40748
Owais Ayoub <oemollic@gmail.com>
2023-09-28 12:30:26 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '18e5e255-2b8f-4f1e-bedf-0b3c7fff690c';
+export const MODAL_CLOSE_SEC = 2.5;
